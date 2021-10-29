c909b235678ca9d7ebba008590af643ab9a5e333
Martin Fimia <martin.fimia.osuna@hotmail.com>
2021-10-29 07:13:16 +0200

    video grid cropped. easy mode implemented so user can see semi transparent background to fix the image

--- main.js
+++ main.js
@@ -2,7 +2,8 @@ let VIDEO = null;
 let CANVAS = null;
 let CONTEXT = null;
 let SCALER = 0.8;
-let SIZE = { x: 0, y: 0, width: 0, height: 0 };
+let SIZE = { x: 0, y: 0, width: 0, height: 0, rows: 3, columns: 3 };
+let PIECES = [];
 
 const main = () => {
   CANVAS = document.getElementById("myCanvas");
@@ -17,7 +18,8 @@ const main = () => {
 
       VIDEO.onloadeddata = () => {
         handleResize();
-        window.addEventListener('resize', handleResize);
+        window.addEventListener("resize", handleResize);
+        initializePieces(SIZE.rows, SIZE.columns);
         updateCanvas();
       };
     })
@@ -27,21 +29,77 @@ const main = () => {
 };
 
 const handleResize = () => {
-    CANVAS.width = window.innerWidth;
-    CANVAS.height = window.innerHeight;
-    let resizer =
-          SCALER *
-          Math.min(
-            window.innerWidth / VIDEO.videoWidth,
-            window.innerHeight / VIDEO.videoHeight
-          );
-        SIZE.width = resizer * VIDEO.videoWidth;
-        SIZE.height = resizer * VIDEO.videoHeight;
-        SIZE.x = window.innerWidth / 2 - SIZE.width / 2;
-        SIZE.y = window.innerHeight / 2 - SIZE.height / 2;
-}
+  CANVAS.width = window.innerWidth;
+  CANVAS.height = window.innerHeight;
+  let resizer =
+    SCALER *
+    Math.min(
+      window.innerWidth / VIDEO.videoWidth,
+      window.innerHeight / VIDEO.videoHeight
+    );
+  SIZE.width = resizer * VIDEO.videoWidth;
+  SIZE.height = resizer * VIDEO.videoHeight;
+  SIZE.x = window.innerWidth / 2 - SIZE.width / 2;
+  SIZE.y = window.innerHeight / 2 - SIZE.height / 2;
+};
 
 const updateCanvas = () => {
+  CONTEXT.clearRect(0, 0, CANVAS.width, CANVAS.height);
+  CONTEXT.globalAlpha=0.5;
   CONTEXT.drawImage(VIDEO, SIZE.x, SIZE.y, SIZE.width, SIZE.height);
+  CONTEXT.globalAlpha=1;
+  
   window.requestAnimationFrame(updateCanvas);
+  for (let i = 0; i < PIECES.length; i++) {
+    PIECES[i].draw(CONTEXT);
+  }
 };
+
+const initializePieces = (rows, cols) => {
+  SIZE.rows = rows;
+  SIZE.columns = cols;
+  PIECES = [];
+  for (let i = 0; i < SIZE.rows; i++) {
+    for (let j = 0; j < SIZE.columns; j++) {
+      PIECES.push(new Piece(i, j));
+    }
+  }
+};
+
+const randomizePieces = () => {
+  for (let i = 0; i < PIECES.length; i++) {
+    let loc = {
+      x: Math.random() * (CANVAS.width - PIECES[i].width),
+      y: Math.random() * (CANVAS.height - PIECES[i].height),
+    };
+    PIECES[i].x = loc.x;
+    PIECES[i].y = loc.y;
+  }
+};
+
+class Piece {
+  constructor(rowIndex, colIndex) {
+    this.rowIndex = rowIndex;
+    this.colIndex = colIndex;
+    this.x = SIZE.x + (SIZE.width * this.colIndex) / SIZE.columns;
+    this.y = SIZE.y + (SIZE.height * this.rowIndex) / SIZE.rows;
+    this.width = SIZE.width / SIZE.columns;
+    this.height = SIZE.height / SIZE.rows;
+  }
+  draw(context) {
+    context.beginPath();
+    context.drawImage(
+      VIDEO,
+      (this.colIndex * VIDEO.videoWidth) / SIZE.columns,
+      (this.rowIndex * VIDEO.videoHeight) / SIZE.rows,
+      VIDEO.videoWidth / SIZE.columns,
+      VIDEO.videoHeight / SIZE.rows,
+      this.x,
+      this.y,
+      this.width,
+      this.height
+    );
+    context.rect(this.x, this.y, this.width, this.height);
+    context.stroke();
+  }
+}
