8a071388a888729de16e54c969c5942a44afd0d2
Martin Fimia <martin.fimia.osuna@hotmail.com>
2021-10-29 07:29:35 +0200

    function created to be able to drag and drop pieces

--- main.js
+++ main.js
@@ -4,10 +4,12 @@ let CONTEXT = null;
 let SCALER = 0.8;
 let SIZE = { x: 0, y: 0, width: 0, height: 0, rows: 3, columns: 3 };
 let PIECES = [];
+let SELECTED_PIECE = null;
 
 const main = () => {
   CANVAS = document.getElementById("myCanvas");
   CONTEXT = CANVAS.getContext("2d");
+  addEventListeners();
 
   let promise = navigator.mediaDevices.getUserMedia({ video: true });
   promise
@@ -28,6 +30,50 @@ const main = () => {
     });
 };
 
+const addEventListeners = () => {
+  CANVAS.addEventListener("mousedown", onMouseDown);
+  CANVAS.addEventListener("mousemove", onMouseMove);
+  CANVAS.addEventListener("mouseup", onMouseUp);
+};
+
+const onMouseDown = (evt) => {
+  SELECTED_PIECE = getPressedPiece(evt);
+  if (SELECTED_PIECE != null) {
+    SELECTED_PIECE.offset = {
+      x: evt.x - SELECTED_PIECE.x,
+      y: evt.y - SELECTED_PIECE.y,
+    };
+  }
+};
+
+const onMouseMove = (evt) => {
+  if (SELECTED_PIECE != null) {
+    SELECTED_PIECE.x = evt.x - SELECTED_PIECE.offset.x;
+    SELECTED_PIECE.y = evt.y - SELECTED_PIECE.offset.y;
+  }
+};
+
+const onMouseUp = (evt) => {
+  if (SELECTED_PIECE.isClose()) {
+    SELECTED_PIECE.snap();
+  }
+  SELECTED_PIECE = null;
+};
+
+const getPressedPiece = (loc) => {
+  for (let i = 0; i < PIECES.length; i++) {
+    if (
+      loc.x > PIECES[i].x &&
+      loc.x < PIECES[i].x + PIECES[i].width &&
+      loc.y > PIECES[i].y &&
+      loc.y < PIECES[i].y + PIECES[i].height
+    ) {
+      return PIECES[i];
+    }
+  }
+  return null;
+};
+
 const handleResize = () => {
   CANVAS.width = window.innerWidth;
   CANVAS.height = window.innerHeight;
@@ -45,10 +91,10 @@ const handleResize = () => {
 
 const updateCanvas = () => {
   CONTEXT.clearRect(0, 0, CANVAS.width, CANVAS.height);
-  CONTEXT.globalAlpha=0.5;
+  CONTEXT.globalAlpha = 0.5;
   CONTEXT.drawImage(VIDEO, SIZE.x, SIZE.y, SIZE.width, SIZE.height);
-  CONTEXT.globalAlpha=1;
-  
+  CONTEXT.globalAlpha = 1;
+
   window.requestAnimationFrame(updateCanvas);
   for (let i = 0; i < PIECES.length; i++) {
     PIECES[i].draw(CONTEXT);
